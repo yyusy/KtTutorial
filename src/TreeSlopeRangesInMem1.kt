aeed262b1ece943d05355fa90d00a3a54a9811e4
yyusy <yusy@mail.ru>
2021-12-24 16:19:19 +0300

    Revert "intial KtTutorial"
    This reverts commit 347cbf76e818e5ef0e52a735cfc73987fe09e59c.

--- src/TreeSlopeRangesInMem1.kt
+++ src/TreeSlopeRangesInMem1.kt
@@ -1,5 +1,6 @@
 fun main() {
     val treeMark = '#'
+    // 1 to 1, 1 to 3, 1 to 5, 1 to 7, 1 to 2
     val l = System.`in`.bufferedReader().readLines()
     listOf(1 to 1, 1 to 3, 1 to 5, 1 to 7, 2 to 1).map { step ->
         (0..l.lastIndex step step.first).mapIndexed { i, y -> y to (step.second * i % l[0].length) }
--- src/VerifyPass.kt
+++ src/VerifyPass.kt
@@ -1,20 +1,22 @@
-fun main() {
-    System.`in`.bufferedReader().useLines { lines ->
-        lines.filter { it.isNotBlank() }
+fun main(args: Array<String>) {
+    System.`in`.bufferedReader().readLines().filter { it.isNotBlank() }
         .map { it.split(' ', '-') }
-        .count {
-            //isValidRange(it[0].toInt()..it[1].toInt(), it[2][0], it[3])
-            isValidPosition(it[0].toInt() - 1 to it[1].toInt() - 1, it[2][0], it[3])
-                .also { r -> println("${it[3]} :$r") }
-
+        .filter { it.size >= 4 }
+        .map {
+            //PassEntry(it[0].toInt()..it[1].toInt(), it[2][0], it[3])
+            PassEntry(it[0].toInt() -1..it[1].toInt()-1 step it[1].toInt()-1, it[2][0], it[3])
         }
+        .count { it.isValidPosition1() }
         .also { println("Valid : $it") }
-    }
 }
 
-private fun isValidRange(r: IntProgression, ch: Char, toVerify: String) = toVerify.count { it == ch } in r
+data class PassEntry(val r: IntProgression, val ch: Char, val toVerify: String) {
+    fun isValidRange() = toVerify.sumOf { if (it == ch) 1.toInt() else 0 } in r
 
-private fun isValidPosition(r: Pair<Int, Int>, ch: Char, toVerify: String) =
-    (toVerify[r.first] == ch).xor(toVerify[r.second] == ch)
+    fun isValidPosition() = (toVerify.getOrNull(r.first - 1)?.equals(ch) == true)
+        .xor(toVerify.getOrNull(r.last - 1)?.equals(ch) == true)
 
+    fun isValidPosition1() = toVerify.filterIndexed{ i, c -> i in r && c.equals(ch) }
+        .count() == 1
+    }
 
