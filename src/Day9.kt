8380bc2cb2961e4d4a89a6b4812f98f16206714c
yyusy <yusy@mail.ru>
2022-01-14 14:09:43 +0300

    fixed Day9

--- src/Day9.kt
+++ src/Day9.kt
@@ -5,14 +5,14 @@ import kotlin.test.assertEquals
 fun main() {
     val windowSize = 25
     val res = File("Day9Input.txt").useLines {
-        invalidNumber(it, windowSize)
+        findInvalidNumber(it, windowSize)
     }
     println(res)
     assertEquals(18272118, res)
 }
 
 @ExperimentalStdlibApi
-fun invalidNumber(it: Sequence<String>, windowSize: Int) =
+fun findInvalidNumber(it: Sequence<String>, windowSize: Int) =
     it.map { it.toLong() }.asSequence()
         .windowed(windowSize + 1) { l ->
             l.last().takeIf { l.take(windowSize).toPairs().count { it.first + it.second == l.last() } == 0 }
--- src/Day9Part2.kt
+++ src/Day9Part2.kt
@@ -1,10 +1,26 @@
 import java.io.File
+import kotlin.test.assertEquals
 
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
     val windowSize = 25
-    val res = File("Day9Input.txt").useLines {
-        invalidNumber(it, windowSize)
+    val expectedSum = File("Day9Input.txt").useLines {
+        findInvalidNumber(it, windowSize)
     }
-    println(res)
+    println("Find sum of : $expectedSum")
+    var found = false
+    val res = ArrayDeque<Long>()
+    File("Day9Input.txt").useLines {
+        for (i in it.map { it.toLong() }) {
+            res.addLast(i)
+            while (res.sum() > expectedSum) res.removeFirst()
+
+            if (res.sum() == expectedSum) {
+                found = true; break
+            }
+        }
+    }
+    println("${if (found) "Found : " else "Not found. "}, $res")
+    println("min : ${res.minOrNull()} + max : ${res.maxOrNull()} = ${(res.minOrNull() ?: 0) + (res.maxOrNull() ?: 0)}")
+    assertEquals(2186361, (res.minOrNull() ?: 0) + (res.maxOrNull() ?: 0))
 }
